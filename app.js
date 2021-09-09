7db09690c51055406bc502619b0c3e444eb2405e
raghav6202 <raghavchandravs@gmail.com>
2021-09-09 22:40:03 +0530

    style:css

--- app.js
+++ app.js
@@ -8,16 +8,23 @@ const calculateBtn = document.querySelector("#calculate-btn");
 
 const outputDisplay = document.querySelector("#output-display");
 
+const mainSpace = document.querySelector("main");
+
 
 function calculateLossOrProfit (initial , quantity , current)
 {
+
+   
 
 if(initial > current)
 {
    var loss = (initial - current) * quantity;
    var lossPercentage = (loss/initial) * 10;
 
-   console.log("loss is " + loss + " loss percentage is " + lossPercentage + "%")
+   mainSpace.style.backgroundColor= 'red';
+
+
+   outputDisplay.innerText = "Ohhho No!! You Incured A Loss \n Your loss is " + loss.toFixed(2) + " loss percentage is " + lossPercentage.toFixed(2) + "%";
 }
 
 else if(current > initial)
@@ -26,16 +33,32 @@ else if(current > initial)
     var profit = (current - initial) * quantity;
     var profitPercentage = (profit/initial) * 10;
 
-    console.log("profit is " + profit + " profit percentage is " + profitPercentage + "%")
+  
+
+    mainSpace.style.backgroundColor = 'green';
+
+    outputDisplay.innerText = "Yayy !!!! You made a profit \n Your profit is " + profit.toFixed(2) + " profit percentage is " + profitPercentage.toFixed(2)  + "% "
     
 }
 
 else
 {
-
-    console.log("Neither Loss nor Profit")
+    mainSpace.style.backgroundColor = 'yellow';
+   outputDisplay.innerText = "You neither made a profit nor incurred a loss.";
 
 }
 }
 
-calculateLossOrProfit(10,10,10)
+function clickHandler()
+{
+
+    var initP = initialPrice.value;
+    var qnty = stocksQuantity.value;
+    var curr = currentPrice.value;
+
+    calculateLossOrProfit(initP, qnty , curr);
+
+
+}
+
+calculateBtn.addEventListener('click', clickHandler)
