51c6d7cb8b4fb508fcfb63512f659e0dfc0a3c0f
raghav6202 <raghavchandravs@gmail.com>
2021-09-09 23:31:36 +0530

    bug:fix

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function calculateLossOrProfit (initial , quantity , current)
 if(initial > current)
 {
    var loss = (initial - current) * quantity;
-   var lossPercentage = (loss/initial) * 100;
+   var lossPercentage = (loss / initial) * 100;
 
    if(loss === 0)
    {
@@ -39,7 +39,7 @@ else if(current > initial)
 {
 
     var profit = (current - initial) * quantity;
-    var profitPercentage = (profit/initial) * 100;
+    var profitPercentage = (profit / initial) * 100;
 
   if(profit === 0)
   {
